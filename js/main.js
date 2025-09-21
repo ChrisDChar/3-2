077ee5845583f2a8e469d384625c9c0d1d8ef150
ChrisDChar <zsultanova425@gmail.com>
2025-09-22 00:57:58 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,87 @@
+// 1
+
+// let A = 8;
+
+// console.log(A % 2 === 0);
+
+
+// 2 
+
+// let A = 2;
+// let B = -5;
+
+// console.log(A >= 0 && B < -2);
+
+
+// 3
+
+// let A = 2;
+// let B = 3;
+// let C = 5;
+
+// console.log(A < B && B < C || A > B && B > C);
+
+
+// 4
+
+// let A = 7;
+// let B = 9;
+
+// console.log(A % 2 === 1 && B % 2 === 1);
+
+
+// 5
+
+// let A = 3;
+// let B = 8;
+
+// console.log(A % 2 === 1 || B % 2 === 1);
+
+
+// 6
+
+// let A = 5;
+// let B = 8;
+
+// console.log(A % 2 !== B % 2);
+
+
+// 7
+
+// let A = 2;
+// let B = 5;
+// let C = 4;
+
+// console.log(A > 0 && B > 0 && C > 0);
+
+
+// 8
+
+// let A = -8;
+// let B = -4;
+// let C = 6;
+
+// console.log(A > 0 || B > 0|| C > 0);
+
+
+// 10
+
+// let A = 22;
+
+// console.log(A >= 10 && A <= 99 && A % 2 === 0);
+
+
+// 11
+
+// let A = 333;
+
+// console.log(A >= 100 && A <= 999 && A % 2 === 1);
+
+
+// 12 
+
+// let A = 12;
+// let B = -12;
+// let C = 4;
+ 
+// console.log(A === -B || A === -C || B === -C);
